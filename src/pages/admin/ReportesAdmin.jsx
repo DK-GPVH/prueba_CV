247d726cd24a0c7dd7b0071f7989beb653396040
SaulLudena <cornejo758@gmail.com>
2022-10-11 11:30:19 -0500

    modificando el packageJson

--- src/pages/admin/ReportesAdmin.jsx
+++ src/pages/admin/ReportesAdmin.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function ReportesAdmin() {
   return (
-    <div>ReportesAdmin</div>
+    <div className="container">
+      <div>
+        <div>Filtrar por</div>
+        <div>Tabla</div>
+      </div>
+      <div></div>
+    </div>
   )
 }
